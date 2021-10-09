a1453f2bd6725061e4518bd876be4ff5f37b7cfe
samuelz1231 <samuelz01@hotmail.com>
2021-10-09 14:25:20 +0800

    Create Exercise4_Tongthong_T.py

---
+++ Exercise4_Tongthong_T.py
@@ -0,0 +1,10 @@
+FE = 89
+GB = 75
+Intro_to_com_sys = 79
+CP = 97
+
+print('Your Score')
+print('Foundation English =',FE)
+print('General Business =',GB)
+print('Introduction to Computer Systems =',Intro_to_com_sys)
+print('Computer Programming =',CP)
